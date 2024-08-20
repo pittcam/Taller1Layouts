efade8f64ff320938d758f4edecf572bd15dce6b
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-08-19 23:38:31 -0500

    Cambios 1

---
+++ app/src/main/java/com/example/taller1layouts/logica/CountryList.kt
@@ -0,0 +1,5 @@
+package com.example.taller1layouts.logica
+
+class CountryList {
+
+}
---
+++ app/src/main/java/com/example/taller1layouts/modelo/Country.kt
@@ -0,0 +1,20 @@
+package com.example.taller1layouts.modelo
+
+data class Country(
+    val name: String,
+    val alpha2Code: String,
+    val alpha3Code: String,
+    val nativeName: String,
+    val region: String,
+    val subRegion: String,
+    val latitude: Double,
+    val longitude: Double,
+    val area: Int,
+    val numericCode: Int,
+    val nativeLanguage: String,
+    val currencyCode: String,
+    val currencyName: String,
+    val currencySymbol: String,
+    val flag: String,
+    val flagPng: String
+)
